977cf434649286b7766beb50e166d6efd801e631
leaxz <98172741+thaliafahira@users.noreply.github.com>
2024-11-19 20:47:45 +0700

    .

---
+++ app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Depends
+from routers import secure, public
+from auth import get_user
+
+app = FastAPI()
+
+app.include_router(
+    public.router,
+    prefix="/api/v1/public"
+)
+app.include_router(
+    secure.router,
+    prefix="/api/v1/secure",
+    dependencies=[Depends(get_user)]
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
