843e36c6043960c97cd4ffe3a900022de33ab5b6
leaxz <98172741+thaliafahira@users.noreply.github.com>
2024-11-24 10:53:58 +0700

    halo

--- app/main.py
+++ app/main.py
@@ -1,13 +1,26 @@
 from fastapi import FastAPI, Depends
-from routers import secure, public
+from typing import Optional, List
+from enum import Enum
+from datetime import datetime
+from pydantic import BaseModel
+from router import secure, public
 from auth import get_user
 
-app = FastAPI()
+app = FastAPI(
+    title="digital wedding invitation",
+    description="Wedding invitation",
+    version="1.0",
+    contact={
+        "name": "Thalia",
+        "email": "thaliafahira@gmail.com"
+    }
+)
 
 app.include_router(
     public.router,
     prefix="/api/v1/public"
 )
+
 app.include_router(
     secure.router,
     prefix="/api/v1/secure",
@@ -15,5 +28,12 @@ app.include_router(
 )
 
 @app.get("/")
-async def root():
-    return {"message": "Hello I'm Thalia"}
+def read_root():
+    return {
+        "message": "Hello World",
+        "project": "Wedding API",
+        "version": "1.0",
+        "developer": "thaliafahira",
+        "description": "Wedding invitation",
+        "last_updated": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }
